45b77955f7d14801b2125ec49105264a763cc29e
ksharm-02 <kartik.sharma@prgx.com>
2019-10-08 21:02:31 +0530

    Added Header Component

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <the-header></the-header>
+    <!-- <router-view /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TheHeader from "./components/TheHeader.vue";
+
+@Component({
+  components: {
+    TheHeader
+  }
+})
+export default class App extends Vue {}
+</script>
