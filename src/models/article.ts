bdd7f9c56be4a189d5a9a1a92534738ca8a8d822
ksharm-02 <kartik.sharma@prgx.com>
2019-10-13 22:49:29 +0530

    Added article interface

---
+++ src/models/article.ts
@@ -0,0 +1,16 @@
+import { User } from "./user";
+
+export interface Article {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  favorited: boolean;
+  favoritesCount: number;
+  author: {
+    following: false;
+  } & User;
+}
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,8 @@
-/*
-bio: null
-createdAt: "2019-10-11T10:56:06.819Z"
-email: "kartik.sharma@nagarro.com"
-id: 70813
-image: null
-token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NzA4MTMsInVzZXJuYW1lIjoia2FydGlrc2hhcm1hIiwiZXhwIjoxNTc1OTc1MzY2fQ.UBMZ7T2oRcymL-NjKa_jQOeQspzRBhZ2AAZFaZkd55A"
-updatedAt: "2019-10-11T10:56:06.824Z"
-username: "kartiksharma"
-*/
-
 export interface User {
   bio: string;
   email: string;
   id: number;
   image: string;
   token: string;
-  updatedAt: Date;
   username: string;
 }
